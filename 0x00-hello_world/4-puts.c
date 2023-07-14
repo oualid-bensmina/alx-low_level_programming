78f2c6264aa15e467939c4e8692cafbfb67bc8ac
oualid-bensmina <bs1oualid@example.com>
2023-07-14 05:08:29 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 
 
 /**
- * main - A program that prints a line with puts function
- * Return: 0 (Success)
+ * main - prints "Programming is like building a miltilingual
+ * puzzle followed by a newline.
+ * Return Always 0.
  */
 
 int main(void)
