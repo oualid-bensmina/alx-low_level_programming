8aa143921d06a6de4a7ac1ba1d3a66bd31cc832c
oualid-bensmina <bs1oualid@example.com>
2023-07-14 05:10:09 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,10 @@
 /**
  * main - prints "Programming is like building a miltilingual
  * puzzle followed by a newline.
- * Return Always 0.
+ * Return: Always 0.
  */
 
 int main(void)
-
 {
 	puts("\"Programing is like building a multilingual puzzle");
 	return (0);
