33ac17b04c17f07e84265cfc4c04ea882d4ea0c0
oualid-bensmina <bs1oualid@example.com>
2023-07-16 06:48:17 -0700

    create

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabitic
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	cgar c;
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
